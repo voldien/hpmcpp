815d9c67388e64e94305cad6a51281e0f42fb6ce
Valdemar Lindberg <voldiekami@gmail.com>
2021-03-30 19:56:51 +0200

    Removed source files and update cmake

--- src/hpmcpp.cpp
+++ src/hpmcpp.cpp
@@ -8,7 +8,3 @@
 #include <hpm.h>
 
 using namespace LIBHPM;
-
-// int Hpm::init(HPMSIMD hpmSIMD) { return hpm_init((unsigned int)hpmSIMD); }
-
-// bool Hpm::isSupported(HPMSIMD SIMD) { return hpm_support_cpu_feat(SIMD); }
